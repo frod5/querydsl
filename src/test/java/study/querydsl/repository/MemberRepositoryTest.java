ba9d697013c33b97ab4d2a18cc32ba6fda5363d4
sunwooKim <frod5@kakao.com>
2023-05-25 16:43:25 +0900

    실전! QueryDsl - Spring Data Jpa repository로 변경

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package study.querydsl.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result = memberRepository.findAll();
+        assertThat(result).containsExactly(member);
+
+        List<Member> result1 = memberRepository.findByUsername("member1");
+        assertThat(result1).containsExactly(member);
+    }
+
+}
